aeeb456dd9177ead97906ce21d9885f471bc4f64
SergeiVlasov1 <vlasovsergei1988@gmail.com>
2022-06-04 21:07:42 +0400

    RUR

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -17,6 +17,9 @@ class MobileBankApiTestV1 {
                 .get("/demo/accounts")
                 // Проверки
                 .then()
-                .statusCode(200);
+                .statusCode(200)
+                // static import для JsonSchemaValidator.matchesJsonSchemaInClasspath
+                .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
+        ;
     }
 }
